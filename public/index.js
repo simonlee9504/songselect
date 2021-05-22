c7dbe1c5ddfd7498219873c7a4c3323c37c45405
Simon Lee <simonlee9504@gmail.com>
2021-05-21 22:24:28 -0400

    setup starter files to mess around with index.html

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var x = 1;
+while (x < 10) {
+	console.log("hello!");
+	x = x + 1;
+}
+document.getElementById(1).innerHTML = "hi";
+
+function myFunction() {
+	document.getElementById(1).innerHTML = "boom!";
+}
