d307025b0e0ec31e1c2afd4ba2e871b824cecf42
Simon Lee <simonlee9504@gmail.com>
2021-06-11 13:01:59 -0400

    added client api test using axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,67 +2,39 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+	const [firstCall, setFirstCall] = useState(false);
 	const [fromAPI, setFromAPI] = useState('');
 
 	const callAPI = async () => {
 		const response: Response = await fetch('/ping');
 		const message: string = await response.text();
 		setFromAPI(message);
-	}
+	};
+
+	const fetchRandomSong = () => {
+		axios.get('/api/songs/random_song').then(response => {
+			setFromAPI(response.data);
+		});
+	};
 
 	useEffect(() => {
-		callAPI();
-	});
+		if (!firstCall) {
+			callAPI();
+			setFirstCall(true);
+		}
+	}, [firstCall]);
 
 	return (
 		<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<Counter />
-			<p>
-			{`ExpressJS API Test: ${fromAPI}`}
-			</p>
-			<span>
-			<span>Learn </span>
-			<a
-				className="App-link"
-				href="https://reactjs.org/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				React
-			</a>
-			<span>, </span>
-			<a
-				className="App-link"
-				href="https://redux.js.org/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Redux
-			</a>
-			<span>, </span>
-			<a
-				className="App-link"
-				href="https://redux-toolkit.js.org/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Redux Toolkit
-			</a>
-			,<span> and </span>
-			<a
-				className="App-link"
-				href="https://react-redux.js.org/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				React Redux
-			</a>
-			</span>
-		</header>
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<Counter />
+				<p>{`ExpressJS API Test: ${fromAPI}`}</p>
+				<button onClick={fetchRandomSong}>GET a random song</button>
+			</header>
 		</div>
 	);
 }
